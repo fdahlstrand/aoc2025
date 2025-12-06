6ac980e8a6e7878de918944e07cfaff8d9ae0ef3
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2025-12-06 12:49:09 +0100

    Solve Day 6, part 2

--- day06/main.go
+++ day06/main.go
@@ -27,13 +27,7 @@ func check(err error, msg string) {
 	}
 }
 
-func main() {
-	fmt.Println("Advent of Code / Day 6")
-
-	file, err := os.Open("input/input1.txt")
-	check(err, "Failed to open file")
-	defer file.Close()
-
+func readProblems(file *os.File) []problem {
 	scanner := bufio.NewScanner(file)
 	var rows [][]string = nil
 	for scanner.Scan() {
@@ -62,6 +56,65 @@ func main() {
 		problems = append(problems, cur)
 	}
 
+	return problems
+}
+
+func readCephalopodProblems(file *os.File) []problem {
+	scanner := bufio.NewScanner(file)
+	var rows [][]string = nil
+	for scanner.Scan() {
+		line := scanner.Text()
+		cols := strings.Fields(line)
+		rows = append(rows, cols)
+	}
+
+	widths := make([]int, len(rows[0]))
+	for _, row := range rows {
+		for col, v := range row {
+			widths[col] = max(widths[col], len(v))
+		}
+	}
+
+	file.Seek(0, 0)
+	scanner = bufio.NewScanner(file)
+	values := make([][]string, len(rows[0]))
+	for scanner.Scan() {
+		line := scanner.Text()
+		pos := 0
+		for col, w := range widths {
+			v := line[pos : pos+w]
+			values[col] = append(values[col], v)
+			pos += w + 1
+		}
+	}
+
+	var problems []problem
+	for col, v := range values {
+		width := widths[col]
+		var problem problem
+		for n := range width {
+
+			var b strings.Builder
+			for _, str := range v {
+				switch str[n] {
+				case '+':
+					problem.op = Add
+				case '*':
+					problem.op = Mul
+				default:
+					b.WriteByte(str[n])
+				}
+			}
+			v, err := strconv.Atoi(strings.TrimSpace(b.String()))
+			check(err, "Failed to convert number")
+			problem.values = append(problem.values, v)
+		}
+		problems = append(problems, problem)
+	}
+	return problems
+}
+
+func solve(problems []problem) int {
 	sum := 0
 	for _, p := range problems {
 		var ans int
@@ -71,17 +124,31 @@ func main() {
 			for _, v := range p.values {
 				ans += v
 			}
-			fmt.Println(ans)
 		case Mul:
 			ans = 1
 			for _, v := range p.values {
 				ans *= v
 			}
-			fmt.Println(ans)
 		default:
 			panic("Unexpected operand")
 		}
 		sum += ans
 	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println("Advent of Code / Day 6")
+
+	file, err := os.Open("input/input1.txt")
+	check(err, "Failed to open file")
+	defer file.Close()
+
+	sum := solve(readProblems(file))
 	fmt.Printf("1) Grand total %d\n", sum)
+
+	file.Seek(0, 0)
+	sum = solve(readCephalopodProblems(file))
+	fmt.Printf("2) Grand total %d\n", sum)
 }
